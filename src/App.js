e54653dce93645169e3b41474aefcf532bb3e4b9
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-08-21 15:02:07 -0700

    Added drum audio files + set Tone.js test for triggering samples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from "react";
+import { Sampler } from "tone";
+import C2 from "./resources/808 kit/808-kick.wav"
+import C3 from "./resources/808 kit/808-snare.wav"
 import './App.css';
 
 function App() {
+
+
+  let sampler = useRef(null);
+
+  useEffect(() => {
+    sampler.current = new Sampler(
+      { C2, C3 }
+    ).toDestination()
+  }, [])
+
+  const handleKick = () => sampler.current.triggerAttack("C2")
+  const handleSnare = () => sampler.current.triggerAttack("C3")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Finger Beats</h1>
+      <button onClick={handleKick}>Kick</button>
+      <button onClick={handleSnare}>Snare</button>
     </div>
   );
 }
