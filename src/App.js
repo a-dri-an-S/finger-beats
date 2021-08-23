30e8a103fae14c3ef8d8e6b416c15c8c85702d10
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-08-22 18:42:08 -0700

    changes wav files to mp3s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { Sampler } from "tone";
-import C2 from "./resources/808 kit/808-kick.wav"
-import C3 from "./resources/808 kit/808-snare.wav"
+import C2 from "./resources/808 kit/808-kick.mp3"
+import C3 from "./resources/808 kit/808-snare.mp3"
 import './App.css';
 
 function App() {
@@ -21,8 +21,8 @@ function App() {
   return (
     <div className="App">
       <h1>Finger Beats</h1>
-      <button onClick={handleKick}>Kick</button>
-      <button onClick={handleSnare}>Snare</button>
+      <button onMouseDown={handleKick}>Kick</button>
+      <button onMouseDown={handleSnare}>Snare</button>
     </div>
   );
 }
